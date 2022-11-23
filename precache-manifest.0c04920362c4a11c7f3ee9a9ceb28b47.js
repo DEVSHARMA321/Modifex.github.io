a86c214fe52f832c0d1fc206efeca8241a7687f3
DEVSHARMA321 <104141468+DEVSHARMA321@users.noreply.github.com>
2022-11-24 01:57:21 +0530

    Add files via upload

---
+++ precache-manifest.0c04920362c4a11c7f3ee9a9ceb28b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "449c433e3a9dcef76dba",
+    "url": "/static/css/main.20791b2a.chunk.css"
+  },
+  {
+    "revision": "449c433e3a9dcef76dba",
+    "url": "/static/js/main.449c433e.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "113c9827677a50926503",
+    "url": "/static/js/2.113c9827.chunk.js"
+  },
+  {
+    "revision": "9f01a62a234a6fc127f8",
+    "url": "/static/js/runtime~main.9f01a62a.js"
+  },
+  {
+    "revision": "6d0883f6671dd777b9b1085c76b8b4d3",
+    "url": "/index.html"
+  }
+];
